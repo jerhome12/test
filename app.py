4460532af261ea32e66747298037921b871335ea
Jerhome <jerhomereantaso8@gmail>
2023-06-01 09:57:02 +0800

    up

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
@@ -7,23 +7,24 @@ from selenium.webdriver.chrome.service import Service
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
+    return render_template('index.html')
 
+@app.route('/start_selenium')
+def start_selenium():
     options = Options()
     options.add_experimental_option("detach", True)
 
-    #Install the chrome driver
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+    # Install the chrome driver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.maximize_window()
 
-    #Link to be open
+    # Link to be opened
     driver.get("https://www.digitalocean.com/company/contact/abuse#intrusion")
-    agree = driver.find_element("xpath",'.//*[@id="truste-consent-button"]')
+    agree = driver.find_element_by_xpath('//*[@id="truste-consent-button"]')
     agree.click()
 
-
-
-    return 'Hello, world!'
+    return 'Selenium code started!'
 
 if __name__ == '__main__':
     app.run()
